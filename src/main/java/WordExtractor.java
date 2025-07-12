7ba5b3ed9704b9f859ebc8e9425847cef63bf4cb
Iryna Finenko <irynafinenko@gmail.com>
2025-07-12 18:18:24 +0300

    fixed after review

--- src/main/java/WordExtractor.java
+++ src/main/java/WordExtractor.java
@@ -3,27 +3,22 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class WordExtractor {
-
     public static List<String> extractWWords(String text) {
         if (text == null || text.isBlank()) {
-            return List.of(); // Повертаємо порожній список, якщо текст порожній
+            return List.of();
         }
 
-        return Arrays.stream(text.toLowerCase().split("[\\s\\p{Punct}]+")) // Розбиваємо рядок
-                .filter(word -> word.startsWith("w"))                      // Відбираємо слова на 'w'
-                .sorted()                                                 // Сортуємо природнім чином
-                .collect(Collectors.toList());                            // Перетворюємо на список
+        return Arrays.stream(text.toLowerCase().split("[\\s\\p{Punct}]+"))
+                .filter(word -> word.startsWith("w"))
+                .sorted()
+                .collect(Collectors.toList());
     }
 
     public static void main(String[] args) {
-        // Тести
         System.out.println(extractWWords("Width world Wide web"));
-        // Вивід: [web, wide, width, world]
-
-        System.out.println(extractWWords("WWW? Four-bedroom farmhouse in the countryside. Wave! All of the four double bedrooms are en suite."));
-        // Вивід: [wave, www]
-
-        System.out.println(extractWWords("Тут немає слів на w!"));
-        // Вивід: []
+        System.out.println(
+                extractWWords("WWW? Four-bedroom farmhouse in the countryside. "
+                              + "Wave! All of the four double bedrooms are en suite."));
+        System.out.println(extractWWords("No words here."));
     }
 }
